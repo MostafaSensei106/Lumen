f3eb81bb9035a46cce95e173dd1d20b2fcec9751
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-09-01 01:16:53 +0300

    feat: update dependencies and add new plugins
    - Added new dependencies including flutter_localizations, cached_network_image_ce, flame, and others to pubspec.yaml.
    - Updated dev_dependencies with build_runner, injectable_generator, json_serializable, and more.
    - Registered new plugins audioplayers_windows and flutter_secure_storage_windows in generated_plugin_registrant.cc.
    - Updated generated_plugins.cmake to include new plugins in the build configuration.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import flutter_secure_storage_darwin
+import games_services
+import google_sign_in_ios
+import in_app_purchase_storekit
+import package_info_plus
+import sqflite_darwin
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  SwiftGamesServicesPlugin.register(with: registry.registrar(forPlugin: "SwiftGamesServicesPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  InAppPurchasePlugin.register(with: registry.registrar(forPlugin: "InAppPurchasePlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
